44a8bf276ba8126610ceb19b0313a0ce8a6870a7
Bobby Schade <bschade.18@gmail.com>
2020-01-17 10:14:52 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/simon-game/precache-manifest.9a18023d6ec96791a746e11ab9737924.js"
+  "/simon-game/precache-manifest.133b43b7df035677fd04572fba7a51e8.js"
 );
 
 self.addEventListener('message', (event) => {
